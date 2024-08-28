3ab7b8b916fb3525f614f11c5181abfee402ef4d
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-08-28 20:03:46 +0200

    progress

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+const SignInPAge = () => {
+	return ( 
+		<div>
+			This is an unprotected page
+		</div>
+	 );
+}
+ 
+export default SignInPAge;
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,15 +1,7 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-
 export default function Home() {
   return (
 		<div>
-      <p className='text-3xl font-medium text-sky-700'>
-        Hello World
-      </p>
-      <Button variant='destructive'>
-        Click me
-      </Button>
+      <p >This is a protected page</p>
 		</div>
 	)
 }
