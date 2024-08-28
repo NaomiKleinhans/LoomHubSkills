63a6b811da0f9ab1b2e38691f8f0617b083d6a67
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-08-28 21:31:13 +0200

    auth

---
+++ middleware.ts
@@ -0,0 +1,6 @@
+// WIthout a defined matcher, this one line applies next-auth
+// to the entire project
+export { default } from 'next-auth/middleware'
+
+//Applies next-auth only to matching routes
+export const config = { matcher: ['/extra', '/dashboard'] }
