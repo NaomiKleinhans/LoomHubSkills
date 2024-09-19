de7a73a0536a340fa5b187a97adabc607823bf1b
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-09-19 18:47:22 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 // import { clerkMiddleware } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(['/course/[...index](.*)'])
+const isProtectedRoute = createRouteMatcher(['/course/[id](.*)'])
 
 export default clerkMiddleware((auth,req) => {
   if (isProtectedRoute(req)) auth().protect()
   })
   // export default clerkMiddleware({})
   export const config = {
-    matcher: ['/((?!.*\\..*|_next).*)','/','/(api|trpc)(.*)','/course/[...index]']
+    matcher: ['/((?!.*\\..*|_next).*)','/','/(api|trpc)(.*)','/course/[id]']
 
 }
