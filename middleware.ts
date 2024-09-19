070dd177751ed8766d97114dade2e18999c98146
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-09-19 20:16:05 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
 const isProtectedRoute = createRouteMatcher(['/course/[...index]'])
 
 export default clerkMiddleware((auth, req) => {
 	if (isProtectedRoute(req)) {
+		const { userId } = auth()
+		if (!userId) {
+			// Redirect to login if the user is not authenticated
+			return NextResponse.redirect('/login')
+		}
 		auth().protect() // Protects the route if it's a protected route
 	}
+	return NextResponse.next() // Proceed to the next middleware or route
 })
 
 // Middleware configuration
