635c0c4a253443c172463bd5bd29f4898b274cb4
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-09-19 19:47:28 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,19 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-// import { clerkMiddleware } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(['/course/[id](.*)'])
+const isProtectedRoute = createRouteMatcher(['/course/[...index]'])
 
-export default clerkMiddleware((auth,req) => {
-  if (isProtectedRoute(req)) auth().protect()
-  })
-  // export default clerkMiddleware({})
- export const config = {
-		matcher: [
-			'/', // Root path
-			'/((?!.*\\..*|_next).*)', // Exclude static files and _next assets
-			'/api/(.*)', // Match all API routes
-			'/course/:path*' // Match all routes under /course with dynamic segments
-		]
- }
+export default clerkMiddleware((auth, req) => {
+	if (isProtectedRoute(req)) {
+		auth().protect() // Protects the route if it's a protected route
+	}
+})
+
+// Middleware configuration
+export const config = {
+	matcher: [
+		'/', // Root path
+		'/((?!.*\\..*|_next).*)', // Exclude static files
+		'/api/(.*)', // Match all API routes
+		'/course/:path*' // Match all routes under /course
+	]
+}
