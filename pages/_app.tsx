9775ec3fe22e2b8858704c07af83f7c795e0b403
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-09-20 00:16:55 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 // import { ClerkProvider } from '@clerk/nextjs'
-// import '../styles/globals.css' // Ensure this path is correct
+import '../styles/globals.css' // Ensure this path is correct
 import type { AppProps } from 'next/app' // Import AppProps
 
 function MyApp({ Component, pageProps }: AppProps) {
