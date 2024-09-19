dad12c62a0d730c38841d4c29e4d72b2b8c767b5
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-09-20 00:31:48 +0200

    Revert "update"
    This reverts commit b3f55e18844cd537df3427e168113b560e8b28fd.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app' // Import AppProps
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return 
