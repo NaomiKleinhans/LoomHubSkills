67e7b2768cfd60affb62425fefb531c98c7baab4
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-09-19 21:54:02 +0200

    delete

--- app/api/course.ts
+++
@@ -1,9 +0,0 @@
-// api/course.ts
-import axios from 'axios'
-
-export const fetchCourses = async () => {
-	const res = await axios.get(
-		`${process.env.NEXT_PUBLIC_STRAPI_URL}/courses?populate=*`
-	)
-	return res.data
-}
--- app/api/lesson.ts
+++
@@ -1,9 +0,0 @@
-// api/lesson.ts
-import axios from 'axios'
-
-export const fetchLessons = async (courseId: string) => {
-	const res = await axios.get(
-		`${process.env.NEXT_PUBLIC_STRAPI_URL}/lessons?filters[course][id][$eq]=${courseId}`
-	)
-	return res.data
-}
